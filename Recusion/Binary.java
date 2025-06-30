ec29305e35f5c003e01e3ba66f8aabfe0e12c031
Shivam Vijay Onkar <shivamonkar934@gmail.com>
2025-06-30 21:15:04 +0530

    Added REcursion folder with initial files

---
+++ Recusion/Binary.java
@@ -0,0 +1,25 @@
+package Recusion;
+//#Paytm
+/*
+ * Print all binary string of size N without 
+ * consecutive ones.
+ */
+
+public class Binary {
+    public static void printBinStr(int n, int lastPlace,String str){
+        //base case
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        
+        printBinStr(n-1, 0, str+"0");
+        if(lastPlace == 0){
+            printBinStr(n-1, 1, str+"1");
+        }
+    }
+
+    public static void main(String args[]){
+        printBinStr(3, 0, " ");
+    }
+}
