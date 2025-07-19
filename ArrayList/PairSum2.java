20a3cd1a203fd1b324cc56409c142b43d96e5edf
Shivam onkar <shivamonkar934@gmail.com>
2025-07-20 01:12:13 +0530

    Add files via upload

---
+++ ArrayList/PairSum2.java
@@ -0,0 +1,48 @@
+package ArrayLists;
+
+import java.util.ArrayList;
+
+//Find if any pair in a Sorted & Rotated ArrayList has a target sum.
+public class PairSum2 {
+    // By 2 pointer Approach and modular Arithmetic
+    //Time complexity = O(n)
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+        int bp = -1;
+        int n = list.size();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i + 1)) {
+
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp + 1;
+        int rp = bp;
+
+        while (lp != rp) {
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % n;
+            } else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
+        System.out.println(pairSum2(list, target));
+    }
+}
