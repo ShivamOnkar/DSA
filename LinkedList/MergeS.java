ae4e8dffc42c3e570e9fc9af32ccb12264e6c750
Shivam onkar <shivamonkar934@gmail.com>
2025-07-30 18:21:31 +0530

    Add files via upload

---
+++ LinkedList/MergeS.java
@@ -0,0 +1,117 @@
+package LinkedList;
+
+public class MergeS {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public  Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow; // mid
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+
+    }
+
+    public  Node mergeSort(Node head) {
+        // base case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+        // left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        // merge
+        return merge(newLeft, newRight);
+    }
+
+    public void print() {
+        // Print to verify
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        MergeS ll = new MergeS(); // create object of your custom linked list
+        ll.addFirst(1);
+        ll.addFirst(4);
+        ll.addFirst(2);
+        ll.addFirst(5);
+        ll.addFirst(3);
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
+    }
+}
+
+// O/P : 
+//  3 -> 5 -> 2 -> 4 -> 1 -> null
+//  1 -> 2 -> 3 -> 4 -> 5 -> null
